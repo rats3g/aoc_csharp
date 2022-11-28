3060a88df46f681f3e8005f9458ea97cb6e51c63
Robert Taylor <rats3g@gmail.com>
2022-11-27 21:08:21 -0500

    Setup HTTP client and retrieve input file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,23 @@
-﻿using System.CommandLine;
+﻿using Microsoft.Extensions.Logging;
+using System.CommandLine;
+using System.Net;
+using System.Reflection;
+
+var handler = new HttpClientHandler
+{
+    CookieContainer = new CookieContainer()
+};
+
+var client = new HttpClient(handler);
+      
+var rootDirectory = Path.GetFullPath(Path.Combine(Assembly.GetExecutingAssembly().Location, @"..\..\..\.."));
+
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddConsole();
+});
+
+var logger = loggerFactory.CreateLogger<Program>();
 
 var rootCommand = new RootCommand("Advent of Code in C#");
 
@@ -59,4 +78,66 @@ var dayOption = new Option<int>(
 
 rootCommand.AddOption(dayOption);
 
+var sessionOption = new Option<string>(
+        aliases: new[] { "-s", "--session" },
+        description: "Login session cookie",
+        isDefault: true,
+        parseArgument: result =>
+        {
+            if (result.Tokens.Count != 0)
+            {
+                return result.Tokens[0].Value;
+            }
+
+            var sessionFile = Path.Combine(rootDirectory, "session.txt");
+            if (File.Exists(sessionFile))
+            {
+                return File.ReadAllText(sessionFile);
+            }
+
+            var sessionVariable = Environment.GetEnvironmentVariable("AOC_SESSION");
+            if (sessionVariable != null)
+            {
+                return sessionVariable;
+            }
+
+            result.ErrorMessage = "Specify session cookie using command line argument (--session), file (session.txt), or environment variable (AOC_SESSION)";
+            return string.Empty;
+        }
+    );
+
+rootCommand.AddOption(sessionOption);
+
+rootCommand.SetHandler(async (year, day, session) =>
+{
+    logger.LogInformation("Running Advent of Code {year} - Day {day}", year, day);
+
+    var problemDirectoryInfo = CreateFolderStructure(year, day);
+
+    handler.CookieContainer.Add(new Uri("https://adventofcode.com/"), new Cookie("session", session));
+
+    await DownloadInputFile(problemDirectoryInfo, year, day);
+},
+yearOption, dayOption, sessionOption);
+
 return await rootCommand.InvokeAsync(args);
+
+DirectoryInfo CreateFolderStructure(int year, int day) => Directory.CreateDirectory(Path.Combine(rootDirectory, year.ToString(), day.ToString()));
+
+async Task DownloadInputFile(DirectoryInfo directoryInfo, int year, int day)
+{
+    var inputFile = Path.Combine(directoryInfo.FullName, "input.txt");
+    if (File.Exists(inputFile)) return;
+
+    var url = $"https://adventofcode.com/{year}/day/{day}/input";
+
+    try
+    {
+        var response = await client.GetStringAsync(url);
+        File.WriteAllText(inputFile, response);
+    }
+    catch (Exception exception)
+    {
+        logger.LogError(exception, "Unable to download input file from {url}", url);
+    }        
+}
